6b1a854e49c19e292b1c39dcc1c1af88b657244b
Mihir8829 <mihirkpatel8829@gmail.com>
2024-04-20 21:58:38 -0400

    Added code to MainActivity

--- app/src/main/java/com/example/project_4/MainActivity.kt
+++ app/src/main/java/com/example/project_4/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     private var GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
     private var WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+    private val API_KEY = "";
     private lateinit var binding: ActivityMainBinding
     private lateinit var weatherFragment: WeatherFragment
     private lateinit var cityFragment: CityFragment
