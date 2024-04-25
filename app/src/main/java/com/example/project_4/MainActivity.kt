cfdbd88ddadeda0bd2365208a9400d9494365598
Mihir8829 <mihirkpatel8829@gmail.com>
2024-04-24 23:39:06 -0400

    Now we can use Weather and City Fragments in MainActivity

--- app/src/main/java/com/example/project_4/MainActivity.kt
+++ app/src/main/java/com/example/project_4/MainActivity.kt
@@ -3,22 +3,33 @@ package com.example.project_4
 import androidx.appcompat.app.AppCompatActivity
 import com.example.project_4.databinding.ActivityMainBinding
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
-    private var GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
-    private var WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-    private val API_KEY = "";
+    private var WEATHER_API_URL = "http://api.weatherapi.com/v1"
+    private val API_KEY = "26cf1eec57b1464fb92211836242404"
     private lateinit var binding: ActivityMainBinding
     private lateinit var weatherFragment: WeatherFragment
     private lateinit var cityFragment: CityFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnGetWeather.setOnClickListener {
+        weatherFragment = WeatherFragment()
+        cityFragment = CityFragment()
 
-            //val city = cityFragment.getCity()   Error: City Fragment needs to be instantiated?
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+        transaction.add(R.id.WeatherFragmentContainer, weatherFragment)
+        transaction.add(R.id.CityFragmentContainer, cityFragment)
+        transaction.commit()
+
+        binding.btnGetWeather.setOnClickListener {
+            val city = cityFragment.getCity()
+
         }
     }
 }
